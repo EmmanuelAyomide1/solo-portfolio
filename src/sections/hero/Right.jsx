bbda454e7bdc7ca0373f01d92d86b279d19ee215
EmmanuelAyomide1 <owoeyeemmanuel206@gmail.com>
2025-08-29 17:57:43 +0100

    fix(hero): image stretch on hero section

--- src/sections/hero/Right.jsx
+++ src/sections/hero/Right.jsx
@@ -7,7 +7,11 @@ function Right() {
     <div className="flex min-h-1/2 justify-center md:h-full md:w-1/2">
       <HeroBg />
       <div className="bg-[url('assets/star4.svg'),url('assets/star4.svg'),url('assets/shout.svg')] bg-[length:20px,10px,30px] bg-[position:85%_30%,0%_60%,0%_30%] bg-no-repeat px-5 md:bg-[length:20px,10px,60px] md:bg-[position:85%_30%,0%_60%,0%_30%]">
-        <img src={person} alt="person" className="mt-2 h-full w-auto" />
+        <img
+          src={person}
+          alt="person"
+          className="mt-2 h-full w-auto object-cover"
+        />
       </div>
       <div className="absolute left-1/2 w-full max-w-[910px] -translate-x-1/2">
         <Experience
