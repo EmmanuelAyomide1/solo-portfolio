42ba23821304394b2d24a92ca08e553fcf4f6686
EmmanuelAyomide1 <owoeyeemmanuel206@gmail.com>
2025-08-29 18:10:31 +0100

    fix(hero): image stretch on hero section

--- src/sections/hero/Right.jsx
+++ src/sections/hero/Right.jsx
@@ -10,7 +10,7 @@ function Right() {
         <img
           src={person}
           alt="person"
-          className="mt-2 h-full w-auto object-cover"
+          className="mt-2 h-full w-auto object-contain"
         />
       </div>
       <div className="absolute left-1/2 w-full max-w-[910px] -translate-x-1/2">
