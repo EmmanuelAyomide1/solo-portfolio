a0c83575f9d15bdfba35f2e9347de7c73f1806b1
EmmanuelAyomide1 <owoeyeemmanuel206@gmail.com>
2025-08-29 19:51:43 +0100

    fix(hero): fixed hero background image

--- src/sections/hero/HeroBg.jsx
+++ src/sections/hero/HeroBg.jsx
@@ -1,23 +1,10 @@
 function HeroBg() {
   return (
-    <div className="absolute -z-1 h-200 overflow-x-clip md:-top-15 md:right-0 md:h-250 md:overflow-y-visible">
-      <svg
-        className="relative -left-[82%] h-[80%] w-[310%] rotate-33 md:-left-[91%] md:rotate-0"
-        xmlns="http://www.w3.org/2000/svg"
-        width="997"
-        height="959"
-        viewBox="0 0 997 959"
-        fill="none"
-      >
-        <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
-          d="M467.485 -63L434.505 539.513L-2.73585e-05 958.071L411.941 895.932L1014.75 805.172L689.615 289.441L467.485 -63Z"
-          fill="#1572D3"
-          fill-opacity="0.15"
-        />
-      </svg>
-    </div>
+    <>
+      <div className="absolute -z-1 h-[120%] w-[170%] -translate-x-[15%] rotate-33 overflow-x-clip md:bottom-0 md:block md:h-[110%] md:w-[200%] md:-translate-x-[32%] md:rotate-0 md:overflow-y-visible">
+        <div className="h-full w-full bg-[url('assets/herobg.png')] bg-cover"></div>
+      </div>
+    </>
   );
 }
 
